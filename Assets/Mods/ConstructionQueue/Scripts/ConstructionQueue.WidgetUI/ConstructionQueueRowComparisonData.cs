415bcfd0bf5603926fe13f8e80bbd3b9dddf6d89
Giovanni Piumatti <g.piumatti@whatwapp.com>
2025-03-11 13:59:52 +0100

    feat(construction-queue): include construction site reachability in sorting order

--- Assets/Mods/ConstructionQueue/Scripts/ConstructionQueue.WidgetUI/ConstructionQueueRowComparisonData.cs
+++ Assets/Mods/ConstructionQueue/Scripts/ConstructionQueue.WidgetUI/ConstructionQueueRowComparisonData.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using Timberborn.BuilderPrioritySystem;
 using Timberborn.BuildingsBlocking;
+using Timberborn.BuildingsReachability;
 using Timberborn.ConstructionSites;
 using Timberborn.PrefabSystem;
 
@@ -11,11 +12,13 @@ namespace ConstructionQueue.WidgetUI {
     readonly ConstructionJob _job;
     readonly BuilderPrioritizable _prioritizable;
     readonly PausableBuilding _pausable;
+    readonly ReachableConstructionSite _reachable;
 
     public ConstructionQueueRowComparisonData(ConstructionJob job) {
       _job = job;
       _prioritizable = job.GetComponentFast<BuilderPrioritizable>();
       _pausable = job.GetComponentFast<PausableBuilding>();
+      _reachable = job.GetComponentFast<ReachableConstructionSite>();
       InstantiationOrder = job.GetComponentFast<InstantiatedPrefab>().InstantiationOrder;
     }
     
@@ -27,6 +30,7 @@ namespace ConstructionQueue.WidgetUI {
      * Sort by
      * - unpaused
      * - is on (grounded and unblocked)
+     * - is reachable
      * - has reserved builder
      * - has reserved capacity
      * - material progress
@@ -36,16 +40,18 @@ namespace ConstructionQueue.WidgetUI {
     public void RefreshComparisonScore() {
       var isUnpaused = _pausable.Paused ? 0 : 1;
       var isOn = _job._constructionSite.IsOn ? 1 : 0;
+      var isReachable = _reachable.IsActiveAndUnreachable() ? 0 : 1;
       var reservedBuilder = _job._constructionSite._reservations._builders.Count > 0 ? 1 : 0;
       var reservedCapacity = _job._constructionSite.Inventory.ReservedCapacity().Any() ? 1 : 0;
       var materialProgress = Convert.ToInt32(_job._constructionSite.MaterialProgress * 10);
       
       var priority = (int)_prioritizable.Priority;
-      ComparisonScore = 100000 * isUnpaused
-                        + 10000 * isOn
-                        + reservedBuilder * 1000
-                        + reservedCapacity * 100
-                        + materialProgress * 10
+      ComparisonScore = 1000000 * isUnpaused
+                        + 100000 * isOn
+                        + 10000 * isReachable
+                        + 1000 * reservedBuilder
+                        + 100 * reservedCapacity
+                        + 10 * materialProgress
                         + priority;
     }
 
